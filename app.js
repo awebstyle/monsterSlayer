76b9c10e4050edaef5fa110417694e30ba220d77
awebstyle <gisele@awebstyle.be>
2022-09-12 08:48:25 +0200

    1st methods
    setup of the attackPlayer and attackMonster methods

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
+// get a random int number betwee min and max
+function getRandomValue(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 const app = Vue.createApp({
-    
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  computed: {},
+  methods: {
+    /**
+     * input : monsterHealth
+     * output : monsterHealth - attackValue
+     **/
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    /**
+     * input : playerHealth
+     * output : playerHealth - attackValue
+     **/
+    attackMonster() {
+      const attackValue = getRandomValue(8,15);
+      this.monsterHealth -= attackValue;
+    },
+  },
 });
 
 app.mount('#game');
